49b9a5c910b8bc4a0b94227f3b3ffff56ccbf6c7
Koustav Basak <koustav23042001@gmail.com>
2026-06-15 20:47:36 +0530

    OOPs - Class Methods & Self

--- OOPs/solve.py
+++ OOPs/solve.py
@@ -14,9 +14,13 @@ class Car:
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
+    
+    def full_car_name(self):
+        return (f"Full Car Name = {self.brand} {self.model}")
         
             
 tata_car = Car("Tata", "Nano")
 print(f"Tata Car Brand= {tata_car.brand}")
 print(f"Tata Car Model= {tata_car.model}")
+print(tata_car.full_car_name())
 
