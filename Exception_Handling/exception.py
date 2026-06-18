06f21a2d7ce2438a1ea86f4c72317e18ded6d854
Koustav Basak <koustav23042001@gmail.com>
2026-06-18 21:19:19 +0530

    Exception Handling

---
+++ Exception_Handling/exception.py
@@ -0,0 +1,13 @@
+try:
+    age = int(input("Enter age = "))
+    print("age ", age)
+    list = [2,4,5]
+    print(list[age])
+# except Exception as e:
+#     print(e)
+
+except ValueError:
+    print("Input Wrong Value!")  
+except IndexError:
+    print("Index Out of bounr or maybe wrong pls check")
+
