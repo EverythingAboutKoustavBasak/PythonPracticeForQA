4c7da258d1490dc500fe0781638091ffabe1cf0f
Koustav Basak <koustav23042001@gmail.com>
2026-07-02 20:43:45 +0530

    Completed Step4 and step 5

--- Week03_Project/project_03.py
+++ Week03_Project/project_03.py
@@ -1,6 +1,8 @@
 test_case_list=[]
-test_case_passed_count = 0
-test_case_failed_count = 0
+post_test_case_success_count = 0
+post_test_case_failed_count = 0
+total_post_test_cases_count = 0
+api_status_sent = ""
 
 #custome exception
 class EnterWrongInput(Exception):
@@ -52,10 +54,10 @@ for test_case in range(1, user_max_no_of_test_cases+1):
     test_case_create = create_test_case(test_case_titel, test_case_steps, test_case_expected_value, test_case_actual_value)
     
     # passed/fail Count of TC
-    if test_case_create["status"] == "passed":
-        test_case_passed_count += 1
-    elif test_case_create["status"] == "failed":
-        test_case_failed_count += 1
+    # if test_case_create["status"] == "passed":
+    #     test_case_passed_count += 1
+    # elif test_case_create["status"] == "failed":
+    #     test_case_failed_count += 1
         
     #add the test cases to the lists
     test_case_list.append(test_case_create)
@@ -76,5 +78,73 @@ print("\n[STEP 2] Writing test cases to test_cases.json ... Done")
 with open("test_cases.json", "r") as file:
     loaded_test_cases = json.load(file)
 
+print(loaded_test_cases)
+print(type(loaded_test_cases))
 # print(loaded_test_cases)
 print("\n[STEP 3] Reading test cases back from test_cases.json ... Done")
+
+
+
+
+#step 4 - Posting test cases
+# step-5 update the result
+
+
+print("\n[STEP 4] Posting test cases to JSONPlaceholder API...")
+print("=============================================")
+print("            POSTING REPORT                     ")
+print("=============================================")
+
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+header_content = {
+            "Content-Type": "application/json"
+}
+
+for index, test_case in enumerate(loaded_test_cases, start=1):
+
+    try:
+        res = requests.post(url, 
+                            headers=header_content, 
+                            json=test_case
+                            )
+        
+        response_data = res.json()
+        
+        if res.status_code==201:
+            api_status_sent = "Request Sent Successfully to Server"
+            post_test_case_success_count+=1
+        else:
+            api_status_sent = "Request not sent Server Issue"
+            post_test_case_failed_count+=1
+        
+        print(f"\nTest Case# {index}         : {test_case['title']}")
+        print(f"API Status Sent      : {api_status_sent}")
+        print(f"API Status Code      : {res.status_code}")
+        print(f"API Generated ID     : {response_data['id']}")
+        print("-" * 55)
+        
+        
+    except requests.exceptions.RequestException as e:
+        
+        print(f"\nTest Case {index}: {test_case['title']}")
+        print(f"Status           : {api_status_sent}")
+        print(f"API Status Code  : {res.status_code}")
+        print(f"API Generated ID : {response_data['id']}")
+        print("-" * 55)
+        
+        continue
+    
+    # print(res.status_code)
+    # print(res.text)
+    
+#total count result
+print("==============================================================")
+print(f"Total Posted = {len(loaded_test_cases)} | Req Success = {post_test_case_success_count} | Req Failed = {post_test_case_failed_count}")
+print("==============================================================")
+
+
+#end
+print("Made By KOUSTAV BASAK - Thank you for using!!!")
