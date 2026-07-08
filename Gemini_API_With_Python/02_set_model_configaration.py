b53e367348375101d4692293fa7e5ad10ed6eed3
Koustav Basak <koustav23042001@gmail.com>
2026-07-08 16:30:34 +0530

    add the configaration - system prompt

--- Gemini_API_With_Python/02_set_model_configaration.py
+++ Gemini_API_With_Python/02_set_model_configaration.py
@@ -32,21 +32,52 @@ load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
 model_name = os.getenv("GEMINI_API_MODEL")
-prompt = """
+
+SYSTEM_PROMPT = """
+                You are a Senior QA Engineer.
+
+                Generate:
+                - Functional Test Cases
+                - Negative Test Cases
+                - Boundary Test Cases
+                - Edge Cases
+
+                Return JSON only.
+
+                Include:
+                - Test Case ID
+                - Title
+                - Preconditions
+                - Steps
+                - Expected Result
+                - Priority
+                - Test Type
+"""
+user_prompt1 = """
         wite test cases for login features 
         make sure all functionality should be covered including positive, negative, edge cases
+        """
+user_prompt2 = """
+        wite test cases for the belowuser story 
+        story Description - As Liza (client), I want to be able to click anywhere in a row within Call Center Insights (not just a "Details" button), so that I can navigate to the call's Insights tab more quickly.
+        Acceptance Criteria - Clicking anywhere within a table row navigates the user to that call's Insights/detail view.
+        The existing "Details" button/link continues to function as an alternative entry point.
+        Change is applied consistently across all rows in the Call Center Insights table.
+
         """
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 responses = client.models.generate_content(
     model = model_name,
-    contents = prompt,
+    contents = user_prompt2,
     
     #configaration setup
     config = types.GenerateContentConfig(
+        system_instruction=SYSTEM_PROMPT,
         temperature = 0.1,
         max_output_tokens = 3000
+
     )
     
 )
