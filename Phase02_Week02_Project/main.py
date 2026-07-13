ec35c85f5d43bad3cff24b79e37b075787a1ec42
Koustav Basak <koustav23042001@gmail.com>
2026-07-13 17:11:45 +0530

    Step - 2 for AI Powered Test Case Generator is implemented - Read user story from a .txt file

--- Phase02_Week02_Project/main.py
+++ Phase02_Week02_Project/main.py
@@ -15,6 +15,10 @@ class MissingAPIKeyOrModel(Exception):
     # Raised when the Gemini API key or model name is missing.
     pass
 
+class EmptyUserStory(Exception):
+    #Raised when the file(user story) is empty or missing
+    pass
+    
 
 
 
@@ -22,8 +26,8 @@ class MissingAPIKeyOrModel(Exception):
 # Load environment variables
 load_dotenv()
 
-# Step: 1
-print("[STEP 1] Loading API key and API model from .env... Starting")
+# Step: 1 (	Load API key safely	from .env file)
+print("[STEP 1] Loading API key and API model from .env - Starting...")
 #Reading the values from .env file
 api_key = os.getenv("GEMINI_API_KEY", "").strip() #missing environment variables default to "", and .strip() removes accidental spaces.
 model_name = os.getenv("GEMINI_API_MODEL", "").strip() #missing environment variables default to "", and .strip() removes accidental spaces.
@@ -44,4 +48,29 @@ except MissingAPIKeyOrModel as e:
     sys.exit(1) # sys.exit(0) → Successful termination , sys.exit(1) → Terminated because of an error
 
 print("[STEP 1] Loading API key and API model from .env... Done✅")
+
+#step: 2 (Read user story from a .txt file)
+print("[STEP 2] Reading user story from user_story.txt - Starting...")
+
+user_story_txt_file_pah = "user_story.txt"
+try:
+    # Check whether the file exists
+    if not os.path.exists(user_story_txt_file_pah):
+        raise EmptyUserStory("user_story.txt is missing.")
+
+    # Read the file
+    with open("user_story.txt", "r", encoding="utf-8") as file:
+        user_story_data = file.read().strip()  # And strip() handles whitespace-only files - removes extra whitespace from the beginning and end. also helps detect whitespace-only files:
+
+    # Check whether the file is empty
+    if not user_story_data:
+        raise EmptyUserStory("user_story.txt is empty.")
+
+except EmptyUserStory as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+    
+print("[STEP 2] Reading user story from user_story.txt... Done✅")
+print(user_story_data)
+
 
