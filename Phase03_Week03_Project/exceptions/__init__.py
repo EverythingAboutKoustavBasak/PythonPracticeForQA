596a2d7c26f983a28f95571dd0e19fa1d6d679f2
Koustav Basak <koustav23042001@gmail.com>
2026-08-04 20:25:18 +0530

    Update the comment regarding some knowladge of __init__.py

--- Phase03_Week03_Project/exceptions/__init__.py
+++ Phase03_Week03_Project/exceptions/__init__.py
@@ -3,6 +3,17 @@ __init__.py is a special file that marks a directory as a Python package.
 It is executed when the package is imported and can be used to initialize the package, 
 expose selected classes or functions, and simplify imports.
 
+Now anywhere in your project you simply write(Inside this project, you can write:):
+from exceptions import MissingAPIKey
+instead of
+from exceptions.custom_exceptions import MissingAPIKey
+
+how can another project import it?
+Ans -
+Turn your project into an installable package.(ex - ai_test_case_generator/)
+Then install it: pip install .
+Now any project can do: from ai_test_case_generator.exceptions import MissingAPIKey
+This is how libraries like requests, pydantic, and pytest work.
 
 """
 
