dbbca7582682ccb03dd4c786d059ed5623863b87
Koustav Basak <koustav23042001@gmail.com>
2026-08-10 21:06:05 +0530

    Implementing the init.py for changing the services folder to the package

---
+++ Phase03_Week03_Project/services/__init__.py
@@ -0,0 +1,6 @@
+from .gemini_service import generate_test_cases
+from .api_service import post_test_cases
+from .report_service import (
+    generate_test_case_report,
+    generate_posting_report,
+)
